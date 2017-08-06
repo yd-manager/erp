565ea806e14482eb6cdd83584478e9a9d722b3b6
Ramazan Karagöz <ramazan.karagoz@mebitech.com>
2017-08-06 19:19:36 +0300

    WEB-FRAGMENT integration

---
+++ erp-webapps/erp-stock-webapp/src/main/java/Test.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Ramazan Karagoz on 6.08.2017.
+ */
+@WebServlet("/Test")
+public class Test extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        out.println("TEST DENEME");
+
+        System.out.println("SERVLET STOCK MODÜLÜNÜN İÇİNDE..");
+
+    }
+}
